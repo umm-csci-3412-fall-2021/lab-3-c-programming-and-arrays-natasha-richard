8cfc8d3fe5eb3e4e353347c91f7fd63c9bc6e2a7
Richard Lussier <lussi036@morris.umn.edu>
2021-09-30 11:31:53 -0500

    Implement mergesort into mergesort.c
    Implemented the mergesort algorithm into both the array_merge.c and mergesort.c files. The version inside of array_merge.c is unneeded and will be removed at a later date. There are no memory leaks inside of the mergesort_test file.
    
    Co-authored-by: Natasha-Zebrev <zebre001@morris.umn.edu>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,56 @@
 #include "array_merge.h"
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
+  int sizeOfSizes = sizeof(sizes) / sizeof(int);
+  mergesort(sizes, 0, sizeOfSizes - 2);	
   return sizes;
+}
+
+bool needsSorting(int range) {
+  return range >= 2
+}
+
+void mergeRanges(int* values, int startIndex, int midPoint, int endIndex) {
+  int rangeSize = endIndex - startIndex;
+  int terminationBytes = 8;
+  //Will allocate memory for the correct amount of integers, including the termination character
+  int* destination = malloc(rangeSize*sizeof(int) + terminationBytes);
+  int firstIndex = startIndex;
+  int secondIndex = midPoint;
+  int copyIndex = 0;
+  while(firstIndex < midPoint && secondIndex < endIndex) {
+    if(values[firstIndex] < values[secondIndex]) {
+      destination[copyIndex] = values[firstIndex];
+      firstIndex++;
+    } else {
+      destination[copyIndex] = values[secondIndex];
+      secondIndex++;
+    }
+    copyIndex++;
+  }
+  while(firstIndex < midPoint) {
+    destination[copyIndex] = values[secondIndex];
+    copyIndex++;
+    firstIndex++;
+  }
+  while(secondIndex < endIndex) {
+    destination[copyIndex] = values[secondIndex];
+    copyIndex++;
+    secondIndex++;
+  }
+  for(int i = 0; i < rangeSize; ++i) {
+    values[i + startIndex] = destination[i];
+  }
+  destination[rangeSize + 1] = '\0';
+  free(destination);
+}
+
+void mergesort(int* values, int startIndex, int endIndex) {
+  int rangeSize = endIndex - startIndex;
+  if(needsSorting(rangeSize)) {
+    int midPoint = (startIndex + endIndex) / 2;
+    mergesort(values, midPoint, endIndex);
+    mergesort(values, startIndex, midPoint);
+    mergeRanges(values, startIndex, midPoint, endIndex);
+  }
 }
--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,58 @@
 #include "mergesort.h"
+#include <stdlib.h>
 
-void mergesort(int, int*) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  return;
+//This is a helper method that checks whether the array needs to be sorted based on its current range
+bool needsSorting(int range) {
+  return range >= 2;
+}
+
+//This is the method where all the sorting is done
+void mergeRanges(int* values, int startIndex, int midPoint, int endIndex) {
+  int rangeSize = endIndex - startIndex;
+  //Will allocate the amount of space needed based on the current range
+  int* destination = (int*) malloc(rangeSize*sizeof(int));
+  int firstIndex = startIndex;
+  int secondIndex = midPoint;
+  int copyIndex = 0;
+  while(firstIndex < midPoint && secondIndex < endIndex) {
+    if(values[firstIndex] < values[secondIndex]) {
+      destination[copyIndex] = values[firstIndex];
+      firstIndex++;
+    } else {
+      destination[copyIndex] = values[secondIndex];
+      secondIndex++;
+    }
+    copyIndex++;
+  }
+  while(firstIndex < midPoint) {
+    destination[copyIndex] = values[firstIndex];
+    copyIndex++;
+    firstIndex++;
+  }
+  while(secondIndex < endIndex) {
+    destination[copyIndex] = values[secondIndex];
+    copyIndex++;
+    secondIndex++;
+  }
+  for(int i = 0; i < rangeSize; ++i) {
+    values[i + startIndex] = destination[i];
+  }
+  //freeing the memory
+  free(destination);
+}
+
+//This method recursively calls itself and mergeRanges.
+void mergesortRange(int* values, int startIndex, int endIndex) {
+  int rangeSize = endIndex - startIndex;
+  if(needsSorting(rangeSize)) {
+    int midPoint = (startIndex + endIndex) / 2;
+    mergesortRange(values, midPoint, endIndex);
+    mergesortRange(values, startIndex, midPoint);
+    mergeRanges(values, startIndex, midPoint, endIndex);
+  }
+}
+
+//The actual method being called to sort the given array.
+void mergesort(int length, int* array) {
+  mergesortRange(array, 0, length);
 }
