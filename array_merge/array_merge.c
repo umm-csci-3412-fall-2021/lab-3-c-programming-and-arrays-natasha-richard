5e7c2a4e8d93ec0cdb18d184b3531bf3b8aa538e
Natasha Zebrev <zebre001@morris.umn.edu>
2021-10-04 22:28:14 -0500

    Add array_merge method
    Added the array_merge method. Implemented a helper function to count the
    number of distinct values in the full sorted array. Not all tests pass,
    the tests with duplicates are failing.
    
    Co-authored-by: RichardLussier <lussi036@morris.umn.edu>

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,56 +1,57 @@
 #include "array_merge.h"
+#include "../mergesort/mergesort.c"
 
+
+int countDistinct(int a[], int x){
+int i, j, count = 1;
+for(i =1; i<x; i++){
+	for(j=0; j<i; j++){
+	if(a[i] == a[j])
+		{break;}
+	}
+	if(i == j) {
+	count++;}
+}
+	return count;
+}
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  int sizeOfSizes = sizeof(sizes) / sizeof(int);
-  mergesort(sizes, 0, sizeOfSizes - 2);	
-  return sizes;
+ 
+       	if(num_arrays == 0) {
+	 int* output = (int*) malloc(sizeof(int));
+	 output[0] = 0;
+	       	return output;
+       	}
+  int totalSize = 0;
+  int i;
+  for(i=0; i<num_arrays; i++) {
+ 	 totalSize = totalSize + sizes[i];
+  
+ }
+ 
+int* bigArray = (int*) malloc((totalSize)*sizeof(int));
+int totalCount = 0;
+int n, j;
+  for(n=0; n<num_arrays; n++){
+  	for(j=0; j<sizes[n]; j++){
+	bigArray[totalCount] = values[n][j];
+	totalCount++;
+	}
+  }
+  mergesort(totalSize, bigArray);
+int distinctNums = countDistinct(bigArray, totalSize);
+int* output = (int*) malloc((distinctNums + 1)*sizeof(int));
+output[0] = distinctNums;
+int outputPosition = 1;
+int m;
+for(m=0; m<totalSize-1; m++){
+	if(bigArray[m] != bigArray[m+1]){
+	output[outputPosition] = bigArray[m];
+	outputPosition++;
+	if(m==totalSize-2) {
+	output[outputPosition] = bigArray[totalSize-1];}
+	}
 }
 
-bool needsSorting(int range) {
-  return range >= 2
-}
-
-void mergeRanges(int* values, int startIndex, int midPoint, int endIndex) {
-  int rangeSize = endIndex - startIndex;
-  int terminationBytes = 8;
-  //Will allocate memory for the correct amount of integers, including the termination character
-  int* destination = malloc(rangeSize*sizeof(int) + terminationBytes);
-  int firstIndex = startIndex;
-  int secondIndex = midPoint;
-  int copyIndex = 0;
-  while(firstIndex < midPoint && secondIndex < endIndex) {
-    if(values[firstIndex] < values[secondIndex]) {
-      destination[copyIndex] = values[firstIndex];
-      firstIndex++;
-    } else {
-      destination[copyIndex] = values[secondIndex];
-      secondIndex++;
-    }
-    copyIndex++;
+free(bigArray);
+  return output;
   }
-  while(firstIndex < midPoint) {
-    destination[copyIndex] = values[secondIndex];
-    copyIndex++;
-    firstIndex++;
-  }
-  while(secondIndex < endIndex) {
-    destination[copyIndex] = values[secondIndex];
-    copyIndex++;
-    secondIndex++;
-  }
-  for(int i = 0; i < rangeSize; ++i) {
-    values[i + startIndex] = destination[i];
-  }
-  destination[rangeSize + 1] = '\0';
-  free(destination);
-}
-
-void mergesort(int* values, int startIndex, int endIndex) {
-  int rangeSize = endIndex - startIndex;
-  if(needsSorting(rangeSize)) {
-    int midPoint = (startIndex + endIndex) / 2;
-    mergesort(values, midPoint, endIndex);
-    mergesort(values, startIndex, midPoint);
-    mergeRanges(values, startIndex, midPoint, endIndex);
-  }
-}
